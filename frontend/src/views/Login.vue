9f1b9867604089979bd1857df8adf7bbdb2e2ec5
GodeFvt <110713045+GodeFvt@users.noreply.github.com>
2024-08-25 13:09:33 +0700

    Fix Bug Sprint 1 : Frontend : fix classname

--- frontend/src/views/Login.vue
+++ frontend/src/views/Login.vue
@@ -202,7 +202,7 @@ async function signInOnClick(userLogin) {
                     :class="{
                       'bg-gray-800 hover:bg-gray-900 cursor-pointer':
                         !isNotValid,
-                      'bg-gray-400 cursor-not-allowed disable': isNotValid,
+                      'bg-gray-400 cursor-not-allowed disabled': isNotValid,
                     }"
                     class="itbkk-button-signin w-full text-white font-medium rounded-lg text-sm px-5 py-2.5 text-center focus:outline-none"
                   >
