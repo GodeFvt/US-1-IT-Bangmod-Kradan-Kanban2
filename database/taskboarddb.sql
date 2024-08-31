cb0e900d0f2154b07e377724ddc4dd33d0ecbac1
GodeFvt <110713045+GodeFvt@users.noreply.github.com>
2024-08-31 21:39:18 +0700

    Database : new Design

--- database/taskboarddb.sql
+++ database/taskboarddb.sql
@@ -1,66 +1,108 @@
-SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 
 -- -----------------------------------------------------
 -- Schema taskboard
 -- -----------------------------------------------------
 DROP SCHEMA IF EXISTS `taskboard` ;
+
 CREATE SCHEMA IF NOT EXISTS `taskboard` DEFAULT CHARACTER SET utf8mb3 ;
 USE `taskboard` ;
 
 -- -----------------------------------------------------
+-- Table `taskboard`.`users`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `taskboard`.`users` (
+  `oid` VARCHAR(40) NOT NULL,
+  `name` VARCHAR(45) NOT NULL,
+  `username` VARCHAR(45) NOT NULL,
+  `email` VARCHAR(45) NOT NULL,
+  `role` ENUM('LECTURER', 'STAFF', 'STUDENT') NOT NULL,
+  PRIMARY KEY (`oid`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb3;
+
+
+-- -----------------------------------------------------
+-- Table `taskboard`.`boards`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `taskboard`.`boards` (
+  `boardId` VARCHAR(10) NOT NULL,
+  `boardName` VARCHAR(200) NOT NULL,
+  `isCustomStatus` TINYINT NOT NULL DEFAULT '0',
+  `oid` VARCHAR(40) NOT NULL,
+  PRIMARY KEY (`boardId`),
+  INDEX `fk_board_user_idx` (`oid` ASC) VISIBLE,
+  CONSTRAINT `fk_board_user`
+    FOREIGN KEY (`oid`)
+    REFERENCES `taskboard`.`users` (`oid`))
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb3;
+
+
+-- -----------------------------------------------------
+-- Table `taskboard`.`tasklimits`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `taskboard`.`tasklimits` (
+  `taskLimitId` INT NOT NULL AUTO_INCREMENT,
+  `maximumTask` INT NULL DEFAULT '10',
+  `isLimit` TINYINT(1) NOT NULL DEFAULT '0',
+  `boardId` VARCHAR(10) NOT NULL,
+  PRIMARY KEY (`taskLimitId`),
+  UNIQUE INDEX `idtaskstatus_UNIQUE` (`taskLimitId` ASC) VISIBLE,
+  INDEX `fk_tasklimit_board1_idx` (`boardId` ASC) VISIBLE,
+  CONSTRAINT `fk_tasklimit_board1`
+    FOREIGN KEY (`boardId`)
+    REFERENCES `taskboard`.`boards` (`boardId`))
+ENGINE = InnoDB
+AUTO_INCREMENT = 0
+DEFAULT CHARACTER SET = utf8mb3;
+
+
+-- -----------------------------------------------------
 -- Table `taskboard`.`taskstatus`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `taskboard`.`taskstatus` ;
-
 CREATE TABLE IF NOT EXISTS `taskboard`.`taskstatus` (
   `statusId` INT NOT NULL AUTO_INCREMENT,
   `statusName` VARCHAR(50) NOT NULL,
   `statusDescription` VARCHAR(200) NULL DEFAULT NULL,
   `statusColor` VARCHAR(10) NULL DEFAULT '#828282',
+  `boardId` VARCHAR(10) NULL,
   PRIMARY KEY (`statusId`),
   UNIQUE INDEX `idtaskstatus_UNIQUE` (`statusId` ASC) VISIBLE,
-  UNIQUE INDEX `statusName_UNIQUE` (`statusName` ASC) VISIBLE)
+  INDEX `fk_taskstatus_boards1_idx` (`boardId` ASC) VISIBLE,
+  CONSTRAINT `fk_taskstatus_boards1`
+    FOREIGN KEY (`boardId`)
+    REFERENCES `taskboard`.`boards` (`boardId`))
 ENGINE = InnoDB
-AUTO_INCREMENT = 1
+AUTO_INCREMENT = 0
 DEFAULT CHARACTER SET = utf8mb3;
+
 
 -- -----------------------------------------------------
 -- Table `taskboard`.`tasklists`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `taskboard`.`tasklists` ;
-
 CREATE TABLE IF NOT EXISTS `taskboard`.`tasklists` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `title` VARCHAR(100) NOT NULL,
   `description` VARCHAR(500) NULL DEFAULT NULL,
   `assignees` VARCHAR(30) NULL DEFAULT NULL,
-  `statusId` INT NOT NULL DEFAULT 1,
+  `statusId` INT NOT NULL DEFAULT '1',
   `createdOn` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updatedOn` DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `boardId` VARCHAR(10) NOT NULL,
   PRIMARY KEY (`id`),
-  INDEX `fk_tasklists_taskstatus_idx` (`statusId` ASC) VISIBLE,
+  INDEX `fk_tasklists_board1_idx` (`boardId` ASC) VISIBLE,
+  INDEX `fk_tasklists_taskstatus` (`statusId` ASC) VISIBLE,
+  CONSTRAINT `fk_tasklists_board1`
+    FOREIGN KEY (`boardId`)
+    REFERENCES `taskboard`.`boards` (`boardId`),
   CONSTRAINT `fk_tasklists_taskstatus`
     FOREIGN KEY (`statusId`)
     REFERENCES `taskboard`.`taskstatus` (`statusId`))
 ENGINE = InnoDB
-AUTO_INCREMENT = 1
-DEFAULT CHARACTER SET = utf8mb3;
-
--- -----------------------------------------------------
--- Table `taskboard`.`tasklimit`
--- -----------------------------------------------------
-DROP TABLE IF EXISTS `taskboard`.`tasklimit` ;
-
-CREATE TABLE IF NOT EXISTS `taskboard`.`tasklimit` (
-  `taskLimitId` INT NOT NULL AUTO_INCREMENT,
-  `maximumTask` INT NULL DEFAULT 10,
-  `isLimit` TINYINT(1) NOT NULL DEFAULT 0,
-  PRIMARY KEY (`taskLimitId`),
-  UNIQUE INDEX `idtaskstatus_UNIQUE` (`taskLimitId` ASC) VISIBLE)
-ENGINE = InnoDB
-AUTO_INCREMENT = 1
+AUTO_INCREMENT = 0
 DEFAULT CHARACTER SET = utf8mb3;
 
 SET SQL_MODE=@OLD_SQL_MODE;
--- database/taskdata.sql
+++ database/taskdata.sql
@@ -1,21 +1,8 @@
-INSERT INTO `taskboard`.`taskstatus` (`statusId`,`statusName`, `statusDescription`,`statusColor`) VALUES
-(1,'No Status','A status has not been assigned','#828282'),
-(2,'To Do','The task is included in the project','#e3f20d'),
-(3,'In Progress','The task is being worked on','#1483c8'),
-(4,'Reviewing','The task is being reviewed','#e8814a'),
-(5,'Testing','The task is being tested','#913de6'),
-(6,'Waiting','The task is waiting for a resource','#fa0000'),
-(7,'Done','The task has been completed','#008f1d');
-
-INSERT INTO `taskboard`.`tasklists` (`id`,`title`, `description`,`assignees`, `statusId`,`createdOn`,`updatedOn`) VALUES
-(1,'NS01',null,null, 1,'2024-04-22 09:00:00','2024-05-14 09:00:00'),
-(2,'TD01', null, null, 2,'2024-04-22 09:05:00','2024-05-14 09:10:00'),
-(3,'IP01', null,null, 3,'2024-04-22 09:10:00','2024-05-14 09:20:00'),
-(4,'TD02', null,null, 2,'2024-04-22 09:15:00','2024-05-14 09:30:00'),
-(5,'DO01', null,null, 7,'2024-04-22 09:10:00','2024-05-14 09:40:00'),
-(6,'IP02', null,null, 3,'2024-04-22 09:10:00','2024-05-14 09:50:00');
-
-INSERT INTO `taskboard`.`tasklimit` (`maximumTask`) VALUES ('10');
+INSERT INTO `taskboard`.`taskstatus` (`statusId`,`statusName`, `statusDescription`,`statusColor`,`boardId`) VALUES
+(1,'No Status','A status has not been assigned','#828282',null),
+(2,'To Do','The task is included in the project','#e3f20d',null),
+(3,'In Progress','The task is being worked on','#1483c8',null),
+(4,'Done','The task has been completed','#008f1d',null);
 
 COMMIT;
 
