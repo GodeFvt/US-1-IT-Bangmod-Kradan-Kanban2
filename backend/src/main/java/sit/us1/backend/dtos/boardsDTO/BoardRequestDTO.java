3c4d6818e2245150b745e6594ef5519284777388
GodeFvt <110713045+GodeFvt@users.noreply.github.com>
2024-09-15 01:41:30 +0700

    Backend : Valid Board Name

--- backend/src/main/java/sit/us1/backend/dtos/boardsDTO/BoardRequestDTO.java
+++ backend/src/main/java/sit/us1/backend/dtos/boardsDTO/BoardRequestDTO.java
@@ -1,11 +1,13 @@
 package sit.us1.backend.dtos.boardsDTO;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 
 @Data
 public class BoardRequestDTO {
     @NotBlank
+    @Size(max = 120)
     private String name;
 }
