787db984d7bb0eb9e6e4d8b1b047e405db0fe231
GodeFvt <110713045+GodeFvt@users.noreply.github.com>
2024-09-15 15:13:50 +0700

    database : default status

--- database/taskdata.sql
+++ database/taskdata.sql
@@ -1,7 +1,7 @@
 INSERT INTO `taskboard`.`taskstatus` (`statusId`,`statusName`, `statusDescription`,`statusColor`,`boardId`) VALUES
-(1,'No Status','A status has not been assigned','#828282',null),
+(1,'No Status','The default status','#828282',null),
 (2,'To Do','The task is included in the project','#e3f20d',null),
-(3,'In Progress','The task is being worked on','#1483c8',null),
+(3,'Doing','Being worked on','#1483c8',null),
 (4,'Done','The task has been completed','#008f1d',null);
 
 COMMIT;
