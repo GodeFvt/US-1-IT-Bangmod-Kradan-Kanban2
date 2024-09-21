ccb94d2e9deb7d11f698e1101f903b62e26742e7
jirananyenlab <jiranan.yenl@mail.kmutt.ac.th>
2024-09-22 01:31:02 +0700

    frontend: test cypress 2.2
     and fig bug after tested

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -9,7 +9,7 @@ const disabledSideMenu = ref(false);
 watch(
   () => route.path,
   (newPath, oldPath) => {
-    if (newPath === "/login" || newPath === "/login/" || newPath === "/"  ) {
+    if (newPath === "/login" || newPath === "/login/" || newPath === "/"  || newPath.includes("TaskNotFound") ) {
       disabledSideMenu.value = true;
     } else {
       disabledSideMenu.value = false;
