5eccc347fecaa49bb010c5964743ebabd9e71a7f
GodeFvt <110713045+GodeFvt@users.noreply.github.com>
2024-09-25 01:35:05 +0700

    FrontEnd : SideMenu transition

---
+++ frontend/src/components/icon/index.js
@@ -0,0 +1,21 @@
+export { default as AccountSettingIcon } from './AccountSettingIcon.vue';
+export { default as QuillIcon } from './QuillIcon.vue';
+export { default as AddButton } from './AddButton.vue';
+export { default as AddIcon } from './AddIcon.vue';
+export { default as AlertSquareIcon } from './AlertSquareIcon.vue';
+export { default as BoardIcon } from './BoardIcon.vue';
+export { default as CloseIcon } from './CloseIcon.vue';
+export { default as DeleteIcon } from './DeleteIcon.vue';
+export { default as EditIcon } from './EditIcon.vue';
+export { default as EditTaskIcon } from './EditTaskIcon.vue';
+export { default as HomeIcon } from './HomeIcon.vue';
+export { default as IsLoading } from './IsLoading.vue';
+export { default as MoreActionIcon } from './MoreActionIcon.vue';
+export { default as SettingIcon } from './SettingIcon.vue';
+export { default as SortIcon } from './SortIcon.vue';
+export { default as SharpMenuIcon } from './SharpMenuIcon.vue';
+export { default as SharpSortIcon } from './SharpSortIcon.vue';
+export { default as MoreActionIconD } from './MoreActionIconD.vue';
+
+
+
