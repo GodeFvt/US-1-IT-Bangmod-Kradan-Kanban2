35512d7c8a3d3c3d28d41eb6f0857c310881a88b
GodeFvt <110713045+GodeFvt@users.noreply.github.com>
2024-09-29 22:38:48 +0700

    add CrossOrigin

--- backend/src/main/java/sit/us1/backend/controllers/AuthenticationController.java
+++ backend/src/main/java/sit/us1/backend/controllers/AuthenticationController.java
@@ -12,7 +12,7 @@ import sit.us1.backend.services.AuthenticationService;
 
 import java.io.IOException;
 
-@CrossOrigin(origins = {"http://localhost:5173", "http://ip23us1.sit.kmutt.ac.th", "http://intproj23.sit.kmutt.ac.th"})
+@CrossOrigin(origins = {"http://localhost:5173", "https://ip23us1.sit.kmutt.ac.th", "https://intproj23.sit.kmutt.ac.th"})
 @RestController
 public class AuthenticationController {
     @Autowired
