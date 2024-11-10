250e8c6ea3266c6d8b2625d621955f333b523b0e
GodeFvt <110713045+GodeFvt@users.noreply.github.com>
2024-11-11 04:05:06 +0700

    Backend : fix subject email send

--- backend/src/main/java/sit/us1/backend/services/EmailService.java
+++ backend/src/main/java/sit/us1/backend/services/EmailService.java
@@ -34,7 +34,7 @@ public class EmailService {
 
     public void sendInvitationEmail(String inviterName, String recipientEmail, String boardName, Collaboration.Access access, String boardId) throws MessagingException, IOException {
         String subject = inviterName + " has invited you to collaborate with " + access.toString() + " access right on board " + boardName;
-        String invitationLink = "https://" + hostName  + "us1/board/" + boardId + "/collab/invitations";
+        String invitationLink = "https://" + hostName  + "/us1/board/" + boardId + "/collab/invitations";
 
         // โหลดไฟล์ HTML template
         Path templatePath = Paths.get(new ClassPathResource("templates/invitation_email_template.html").getURI());
